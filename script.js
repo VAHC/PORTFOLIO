af8f522e82b037593c132aa748d888270d5adb7a
VAHC <victor10alha@gmail.com>
2023-08-07 18:52:48 -0300

    'first commit'

---
+++ script.js
@@ -0,0 +1,40 @@
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    } else{
+        document.getElementById("nav").classList = "responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opción
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+
+// Función que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("node");
+        habilidades[3].classList.add("react");
+        habilidades[4].classList.add("sql");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("proyect");
+    }
+}
+// detecto el scrolling para aplicar la animación de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+} 
